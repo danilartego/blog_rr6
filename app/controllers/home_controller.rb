386f820484392bcf46039d65912560655dd476ef
dn.artego <dn.artego@gmail.com>
2024-09-15 23:32:24 +0300

    add article save validates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
+    @welcome = "Добро пожаловать"
+  end
+
+  def about
+    @about = "О сайте"
+  end
+
+  def terms
+    @terms = "Пользовательское соглашение"
   end
 
 end
