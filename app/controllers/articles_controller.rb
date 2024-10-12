45463a7641d3e9f436237ab15b1154fba74cc3a3
dn.artego <dn.artego@gmail.com>
2024-10-12 13:08:09 +0300

    edit, update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,16 +9,32 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
   def new
   end
 
   def create
     @article = Article.new(article_params)
-    if @article.valid?
-      @article.save
+    if @article.save
+      
       redirect_to @article
+      # render action: "show"
     else
       render action: "new"
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: "edit"
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: "home#index"
-  get '/about', to: "home#about"
-  get '/terms', to: "home#terms"
+  get 'about', to: "home#about"
+  get 'terms', to: "home#terms"
 
   resource :contacts, only: [:new, :create], path_names: { new: '' }
   resources :articles
